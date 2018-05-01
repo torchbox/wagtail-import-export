6021f3ff41d518319b0566448c2f44bdc109ebd9
Matt Westcott <matt@west.co.tt>
2018-05-01 12:53:08 +0100

    add skeleton app and menu item

---
+++ wagtailimportexport/wagtail_hooks.py
@@ -0,0 +1,26 @@
+from django.conf.urls import include, url
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from wagtail.admin.menu import MenuItem
+from wagtailimportexport import urls
+from wagtail.core import hooks
+
+
+@hooks.register('register_admin_urls')
+def register_admin_urls():
+    return [
+        url(r'^import/', include(urls, namespace='wagtailimportexport')),
+    ]
+
+
+class ImportMenuItem(MenuItem):
+    def is_shown(self, request):
+        return request.user.is_superuser
+
+
+@hooks.register('register_admin_menu_item')
+def register_import_menu_item():
+    return ImportMenuItem(
+        _('Import'), reverse('wagtailimportexport:index'), classnames='icon icon-download', order=800
+    )
