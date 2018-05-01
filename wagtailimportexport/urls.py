874fb82e6603fb8ad2ba3d99b5f7d2c7dbc8bcba
Neal Todd <neal.todd@torchbox.com>
2018-05-01 16:11:29 +0100

    Initial export API endpoint

--- wagtailimportexport/urls.py
+++ wagtailimportexport/urls.py
@@ -4,5 +4,5 @@ from wagtailimportexport import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^export/(?P<page_id>\d+)/$', views.export, name='export'),
 ]
--- wagtailimportexport/views.py
+++ wagtailimportexport/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+import json
+
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+
+from wagtail.core.models import Page
 
 from wagtailimportexport.forms import ImportForm
 
@@ -15,3 +19,26 @@ def index(request):
     return render(request, 'wagtailimportexport/import.html', {
         'form': form,
     })
+
+
+def export(request, page_id):
+
+    def page_json(page):
+        return {
+            'content': json.loads(page.to_json()),
+            'model': page.content_type.model,
+            'app_label': page.content_type.app_label,
+        }
+
+    try:
+        page = Page.objects.get(id=page_id, live=True).specific
+    except Page.DoesNotExist:
+        return JsonResponse({'error': 'page not found'})
+    payload = {
+        'id': page.id
+    }
+    payload[page.id] = page_json(page)
+    for subpage in page.get_descendants():
+        payload[subpage.id] = page_json(subpage.specific)
+
+    return JsonResponse(payload)
