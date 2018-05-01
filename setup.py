827af56c4f4c1e55e6f124357ffd1f736f7f80eb
Matt Westcott <matt@west.co.tt>
2018-05-01 12:34:35 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='wagtail-import-export',
+    version='0.1',
+    description="Page export from one Wagtail instance into another",
+    author='Torchbox and NHS Digital',
+    author_email='hello@torchbox.com',
+    url='https://github.com/torchbox/wagtail-import-export',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+)
